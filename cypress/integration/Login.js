33615367588a9744812a3bdd8cbcf22addb9aaf7
RufusA1 <rufus.abioritsegbemi@gmail.com>
2021-11-06 19:50:27 +0100

    successfully logged in

--- cypress/integration/Login.js
+++ cypress/integration/Login.js
@@ -4,5 +4,14 @@ it('LoginTest', function(){
 
     cy.visit('https://dev.pace.africa/')
 
+    cy.get(':nth-child(2) > a > .mb-0').click()
+
+    cy.get(':nth-child(3) > .form-control').type('joseph.abios123@gmail.com')
+
+    cy.get(':nth-child(4) > .form-control').type('password')
+
+    cy.get('.btn').click()
+
+
 
 })
